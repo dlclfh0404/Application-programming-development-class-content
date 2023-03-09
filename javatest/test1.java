5fa0cdfbd533ea44e516c3db9a560655b300ec01
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-03-09 15:12:52 +0900

    Add files via upload

---
+++ javatest/test1.java
@@ -0,0 +1,12 @@
+public class test1{
+	public static void main(String[] args){
+	int cnt = 0;
+	int sum = 0;
+	for(int i = 0; i < 10; i++){
+		cnt+=1;
+		sum = sum + cnt;
+		System.out.println("cnt :" + cnt +" sum :" +sum);
+	}
+	System.out.println("sum :"+ sum);
+      }
+}			
