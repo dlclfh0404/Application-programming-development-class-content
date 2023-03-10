d552b74f7c8768945d828c3d5665a858490dd010
LeeHwaJun <106458316+dlclfh0404@users.noreply.github.com>
2023-03-10 09:58:18 +0900

    Add files via upload

---
+++ javatest/S21218_01v2.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class S21218_01v2{
+	public static void main(String[] args) throws Exception{
+		for(int i = 0; i < 10; i++){
+			int keyCode = System.in.read();
+			System.out.print(keyCode + " / ");
+		}
+   }
+}
